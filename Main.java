88e3e963647b7c6a19c734ecb31ba484197950ec
TeaFred <fhtruong100@student.hbuhsd.edu>
2024-11-15 08:44:53 +0000

    asdasdas

--- Main.java
+++ Main.java
@@ -1,17 +1,29 @@
+package mycustompackage;
 import java.util.Scanner;
 
 public class Main
 {
   public static void main(String[] args)
   {
-    // You can test your method here
-    // System.out.println(isPrime(1));
-    // System.out.println(isPrime(17));
+    Scanner sc = new Scanner(System.in);
+    System.out.println(isPrime(1));
+    System.out.println(isPrime(17));
+    System.out.println(isPrime(23));    
+    System.out.println(isPrime(8675309));
+    System.out.println("Please enter an integer: ");
+    int num1 = sc.nextInt();
+    System.out.println(isPrime(num1));
   }
 
   public static boolean isPrime(int N)
   {
-    // TODO: Replace and write your code below
-    return false;
+    for (int i = 2; i < N; i++)
+    {
+      if (N%i == 0)
+      {
+        return false;
+      }
+    }
+    return true;
   }
 }
